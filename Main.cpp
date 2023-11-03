8107c48edc4fe5daaf81c9a59917d152d2247ca7
Zohaib <zrana1214@gmail.com>
2023-11-02 23:33:28 -0400

    error msg for when no input file is provided

--- Main.cpp
+++ Main.cpp
@@ -7,27 +7,41 @@
 
 int main(int argc, char const *argv[])
 {
-
-    ifstream infile {argv[1]};
-    string file = argv[1];
-
-    //throws an error if no input file provided and exits the program
-    if(!infile){
-        cerr << "could not open file: " << argv[1] << endl;
-        exit(1);
-    } else if(argc !=3){
-      //checks for CL prompt: ./simulation [input file] [initial seed]
-        cerr << "Usage: " << argv[0] << " [input file]" << " [(long) initial seed]" <<"\n";
+    // throws an error if no input file is provided and exits the program
+    if (argc < 2)
+    {
+        cerr << "No input file provided" << endl;
+        cerr << "Usage: " << argv[0] << " [input file]"
+             << " [(long) initial seed]"
+             << "\n";
         exit(1);
     }
 
-    //store seed from CL to use in random number generator
+    ifstream infile{argv[1]};
+    string file = argv[1];
+
+    // throws an error if no input file provided and exits the program
+    if (!infile)
+    {
+        cerr << "could not open file: " << argv[1] << endl;
+        exit(1);
+    }
+    else if (argc != 3)
+    {
+        // checks for CL prompt: ./simulation [input file] [initial seed]
+        cerr << "Usage: " << argv[0] << " [input file]"
+             << " [(long) initial seed]"
+             << "\n";
+        exit(1);
+    }
+
+    // store seed from CL to use in random number generator
     long seed = stol(argv[2]);
 
-    Simulation sim(argv[1], seed); //create a simulation
+    Simulation sim(argv[1], seed); // create a simulation
 
-    sim.run(); // run the simulations
-    infile.close(); //close input file
+    sim.run();      // run the simulations
+    infile.close(); // close input file
 
     exit(0);
 }
